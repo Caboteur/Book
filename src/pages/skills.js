6ab595890550bdd556cf89e8aa15c39a79a0f857
Caboteur <caboteur.adrienblanchot@gmail.com>
2018-12-28 16:21:04 +0100

    commit skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import ArticlePreview from '../components/article-preview'
-import metromap from '../images/metromap.svg'
+import metromap from '../images/metromap2.svg'
 import styled from './skills.css'
 
 class Skills extends React.Component {
