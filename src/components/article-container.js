8bd742dc72d147076a600fde6da8e262abc06a4a
Caboteur <caboteur.adrienblanchot@gmail.com>
2019-03-06 14:03:18 +0100

    size

--- src/components/article-container.js
+++ src/components/article-container.js
@@ -13,17 +13,9 @@ export default class RootIndex extends React.Component {
       <div name={this.props.num}>
 
 
-         {this.props.name === "Les Saisons"  ?
-          (
-     <video className="video-container" src={this.props.image} title={this.props.title} type="video/mov" preload controls />
-    )  :  this.props.name === "Consilium" ?
     <img className="img-w-container" src={this.props.image} />
-    :  this.props.name === " Dr Martens"  ?
-    <img className="img-wd-container" src={this.props.image} />:
-      this.props.name ===   "Scandal By Night" ?
-    <img className="img-w-container" src={this.props.image} />: (
-      <img className="img-container" src={this.props.image} />
-    )}
+
+
 
          <h1 className="title">{this.props.name}</h1>
 
